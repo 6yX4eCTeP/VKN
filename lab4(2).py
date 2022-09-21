02de217f523a9117f9a8baa9138ffd77add0e822
Oleksandr Manilyuk <113429737+6yX4eCTeP@users.noreply.github.com>
2022-09-21 08:51:14 +0300

    Update lab4(2).py

--- lab4(2).py
+++ lab4(2).py
@@ -1,8 +1,11 @@
 import math 
+def func1(x1,y1,z1):
+    b=(0.7*math.cos(x1))+math.log10(math.fabs(y1))**(1/5)
+    a=(math.e)**(z1+x1)
+    f=b+a
+    return(f) 
 x=float(input("Введіть х "))
 y=float(input("Введіть y "))
 z=float(input("Введіть z "))
-b=(0.7*math.cos(x))+math.log10(math.fabs(y))**(1/5)
-a=(math.e)**(z+x)
-f=b+a
+f=func1(x,y,z)
 print(f)
