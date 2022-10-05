2453203edf1866e4dc8b6e6a2e38801c61fc1b61
Oleksandr Manilyuk <113429737+6yX4eCTeP@users.noreply.github.com>
2022-10-05 09:37:16 +0300

    Update lab6(1).py

--- lab6(1).py
+++ lab6(1).py
@@ -1,10 +1,10 @@
 import math
-a=int(input("a="))
-b=int(input("b="))
-h=int(input("h="))
+a=float(input("a="))
+b=float(input("b="))
+h=float(input("h="))
 x=a
 for x in range(1, 100):
-    bar1=math.cos(x)/1+math.sin(x)
+    bar1=math.cos(x)/(1+math.sin(x))
     print(bar1)
     if x == b:  
          break
