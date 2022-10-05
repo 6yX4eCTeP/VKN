e18f1260b522be7b6f5ada1195199445a5ef02ad
Oleksandr Manilyuk <113429737+6yX4eCTeP@users.noreply.github.com>
2022-10-05 09:37:50 +0300

    Update lab6(2).py

--- lab6(2).py
+++ lab6(2).py
@@ -1,13 +1,13 @@
 import math
 
-a=int(input("a="))
-b=int(input("b="))
-h=int(input("h="))
+a=float(input("a="))
+b=float(input("b="))
+h=float(input("h="))
 x=a
-spisok = []
+
 while x<b:
-    spisok.append(math.cos(x)/1+math.sin(x))
-    x += h
+    bar1= (math.cos(x)/(1+math.sin(x)))
+    print (bar1)
+    x = x+h
     if x>b:
         break
-print (spisok)
